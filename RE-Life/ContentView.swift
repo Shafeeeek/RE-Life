2cc449789b77ccb8674d1d858bdfa4fdcd36b36e
shafeek <mohmedshii02@gmail.com>
2026-03-05 23:55:29 +0200

    Initial Commit

---
+++ RE-Life/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RE-Life
+//
+//  Created by shafeek macbook pro on 05/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RE-Life/RE_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RE_LifeApp.swift
+//  RE-Life
+//
+//  Created by shafeek macbook pro on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RE_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
